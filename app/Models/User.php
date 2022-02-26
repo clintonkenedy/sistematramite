61884f8e2e12d5537977f8ca50481f619642f5c6
Clinton <clintonventuracondori@gmail.com>
2022-02-26 01:56:15 -0500

    agregamos una migation de doc y su modelo luego agregamos los middleare de satie al kernel y modificamos el model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+//spatie 
+use Spatie\Permission\Traits\HasRoles;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
